2882f67cb6de630ea2eef923867c69034d05f5ee
Patrick Jordan <jordanpatrick@gmail,com>
2023-07-29 16:24:23 +0200

    first commit

---
+++ src/main/java/ch/pjordan/standup/StandupApplication.java
@@ -0,0 +1,13 @@
+package ch.pjordan.standup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/pjordan/standup/StandupApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.pjordan.standup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
